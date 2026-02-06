16d3decc0d064b632efad4e470bdbcb8abad3c25
KakashiCK440 <163229143+KakashiCK440@users.noreply.github.com>
2026-02-06 06:27:08 +0200

    Modify bot response and update run function
    Updated the home response message and renamed the run function to run_web. Adjusted the run_web function to use a configurable port.

--- improved_discord_bot.py
+++ improved_discord_bot.py
@@ -13,13 +13,15 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return "I'm alive!"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
+def run_web():
+    # Use the PORT environment variable provided by your host, default to 8000
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-    t = Thread(target=run)
+    t = Thread(target=run_web)
     t.start()
 
 intents = discord.Intents.default()
